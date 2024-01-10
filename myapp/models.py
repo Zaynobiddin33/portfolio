09143ac7f1b1f59f89a5f11ee8f30217fd31a4c2
Zaynobiddin <zaynobiddinshaxobiddinov9999@gmail.com>
2024-01-10 23:22:53 +0500

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length = 255)
+    url = models.URLField(null = True)
+
+class Mymedia(models.Model):
+    media_name = models.CharField(max_length = 255)
+    url = models.URLField()
+
+class Message(models.Model):
+    name = models.CharField(max_length = 250)
+    email = models.EmailField()
+    text = models.TextField()
---
+++ myapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
+
+# Create your views here.
+
+def main(request):
+    projects = Projects.objects.all()
+    forms = Message.objects.all()
+    medias = Mymedia.objects.all()
+    context = {
+        "forms" : forms,
+        "medias" : medias,
+        "projects" : projects
+    }
+    return render(request, 'data.html', context)
+
+def form(request):
+    name = request.POST['name']
+    email = request.POST['email']
+    text = request.POST['text']
+    Message.objects.create(
+        name = name,
+        email = email,
+        text = text
+    )
+    return render(request, 'new.html')
+
+def mymedia(request):
+    return render(request, 'sm.html')

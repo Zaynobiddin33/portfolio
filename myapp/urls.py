4befb60b694fa667a726b13cd341933ec97a369e
Zaynobiddin <zaynobiddinshaxobiddinov9999@gmail.com>
2024-01-19 23:50:07 +0500

    portfolio update

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,21 @@ from .views import *
 urlpatterns =[
     path('', main),
     path('form', form),
-    path('urls', mymedia)
+    path('urls', mymedia),
+    #dashboard
+    path('dashboard', dash , name = 'dash'),
+    #contact
+    path('contacts', list_contact, name = 'list_contact'),
+    path('contacts/detail/<int:id>/', detail_contact, name = 'detail_contact'),
+    #Mymedia
+    path('medias', medias, name='medias'),
+    path('medias/create', creata_media, name = 'create_media'),
+    path('medias/update/<int:id>/', update_media, name = 'update_media'),
+    path('medias/delete/<int:id>/', delete_media, name = 'delete_media'),
+    #Projects
+    path('projects', projects, name='projects'),
+    path('projects/create', creata_project, name = 'create_project'),
+    path('projects/update/<int:id>/', update_project, name = 'update_project'),
+    path('projects/delete/<int:id>/', delete_project, name = 'delete_project'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpResponse
 
@@ -29,3 +29,103 @@ def form(request):
 
 def mymedia(request):
     return render(request, 'sm.html')
+
+def dash(request):
+    medias = Mymedia.objects.all().count()
+    contacts = Mymedia.objects.all().count()
+    projects = Mymedia.objects.all().count()
+    
+    context = {
+        'medias':medias,
+        'contacts':contacts,
+        'projects':projects
+    }
+    
+    return render(request, 'dashboard/index.html', context)
+
+#CRUD contact
+def list_contact(request):
+    messages = Message.objects.all()
+    context = {
+        "messages": messages
+    }
+    return render(request, 'dashboard/contact/list.html', context)
+
+def detail_contact(request, id):
+    message = Message.objects.get(id=id)
+    context = {
+        "message": message
+    }
+    return render(request, 'dashboard/contact/detail.html', context )
+
+#CRUD Mymedia
+def medias(request):
+    medias = Mymedia.objects.all()
+    context = {
+       'medias': medias 
+    }
+    return render(request, 'dashboard/medias/list.html', context)
+
+
+def creata_media(request):
+
+    if request.method == "POST":
+        name = request.POST['name']
+        url = request.POST['url']
+        Mymedia.objects.create(
+            media_name = name,
+            url = url
+        )
+        return redirect('medias')
+        
+    return render(request, 'dashboard/medias/create.html')
+
+def  update_media(request, id):
+    media = Mymedia.objects.get(id =id)
+    if request.method == 'POST':
+        media.media_name = request.POST['name']
+        media.url = request.POST['url']
+        media.save()
+        return redirect('medias')
+    context = {
+        'media': media
+    }
+    return render(request, 'dashboard/medias/update.html',context)
+
+def delete_media(request, id):
+    Mymedia.objects.get(id=id).delete()
+    return redirect('medias')
+
+#CRUD projects
+def projects(request):
+    projects = Projects.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'dashboard/projects/list.html', context)
+
+def creata_project(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        url = request.POST['url']
+        Projects.objects.create(
+            name = name,
+            url = url
+        )
+    
+    return render(request, 'dashboard/projects/create.html')
+
+def update_project(request, id):
+    project = Projects.objects.get(id = id)
+    if request.method  == "POST":
+        project.name = request.POST['name']
+        project.url = request.POST['url']
+        project.save()
+    context = {
+        'project': project
+    }
+    return render(request, 'dashboard/projects/update.html', context)
+
+def delete_project(request, id):
+   Projects.objects.get(id=id).delete()
+   return redirect('projects')
